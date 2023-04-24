fd6e9c9674fb5ba73e61aa83428b1dfe5fea5919
Anas Saber <anastal2001@yahoo.com>
2023-04-24 13:03:06 +0200

    fetch user by id

--- src/user/auth.service.ts
+++ src/user/auth.service.ts
@@ -2,6 +2,7 @@ import { UserValue } from './user.value';
 import { UserToken } from './user.token';
 
 export interface IAuthService {
+  getUserById: (id: string) => Promise<UserValue>;
   validateUser: (email: string, password: string) => Promise<any>;
   verifyToken: (token: string) => any;
   login: (user: UserValue) => Promise<UserToken>;
